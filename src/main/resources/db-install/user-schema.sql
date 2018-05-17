c76dcae92a407c4ffc670a2915310b15434e29ec
madstriker <suminshakya@gmail.com>
2018-05-17 16:28:05 +0545

    query for creating user

---
+++ src/main/resources/db-install/user-schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tbl_user (
+    user_id int PRIMARY KEY AUTO_INCREMENT,
+    first_name varchar(50),
+    middle_name varchar(50),
+    last_name varchar(50),
+    mobile_number varchar(50),
+    email_address varchar(50),
+    address varchar(50),
+    activation_code int,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+    updated_date TIMESTAMP  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    active boolean
+);
