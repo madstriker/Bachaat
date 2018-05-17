5de1062fbbe234ddcab7b03c6cfb0c1204311f49
nitish shrestha <nitishrestha8848@gmail.com>
2018-05-17 13:09:48 +0545

    create user model

---
+++ src/main/java/com/kat/bachaat/model/User.java
@@ -0,0 +1,110 @@
+package com.kat.bachaat.model;
+
+import java.util.Random;
+
+public class User {
+    private Long userId;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String address;
+    private int activationCode;
+    private Long mobileNumber;
+    private boolean active = false;
+
+    public User() {
+        this.activationCode = new Random().nextInt(900000) + 100000;
+    }
+
+    public User(Long userId, String firstName, String middleName, String lastName, String email, String address, int activationCode, Long mobileNumber, boolean active) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.activationCode = activationCode;
+        this.mobileNumber = mobileNumber;
+        this.active = active;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(int activationCode) {
+        this.activationCode = activationCode;
+    }
+
+    public Long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(Long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "activationCode=" + activationCode +
+                '}';
+    }
+}
