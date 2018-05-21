d55ba53e307be9f9bbc3759cfb5056e5f6fad6fc
madstriker <suminshakya@gmail.com>
2018-05-21 18:00:08 +0545

    Add password field

--- src/main/resources/db-install/user-schema.sql
+++ src/main/resources/db-install/user-schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE tbl_user (
     user_id int PRIMARY KEY AUTO_INCREMENT,
     first_name varchar(50),
     middle_name varchar(50),
+    password varchar(70),
     last_name varchar(50),
     mobile_number varchar(50),
     email_address varchar(50),
